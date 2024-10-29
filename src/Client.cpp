435dd1dcf6d9bb062f73ed1f21906b1df1460896
adilsxn <24adilsonnunes@gmail.com>
2024-10-29 00:13:59 +0000

    add: client implementation

---
+++ src/Client.cpp
@@ -0,0 +1,125 @@
+#include "../includes/Client.hpp"
+
+//TODO: add all the implem of the client class
+
+
+Client::Client(void){
+    return ;
+}
+Client::Client(std::string& nick, std::string& user, int fd)
+:_nickname(nick), _username(user), _fd(fd)
+{
+    return ;
+}
+Client::Client(const Client& src){
+    return ;
+}
+
+Client& Client::operator=(const Client& rhs){
+    return *this;
+}
+
+Client::~Client(void){
+    return ;
+}
+
+const int&         Client::getFd(void)const{
+    return _fd;
+}
+
+const std::string& Client::getHost(void)const{
+    return _hostname;
+}
+
+const std::string& Client::getNick(void)const{ 
+    return _nickname;
+}
+
+const std::string& Client::getUser(void)const{
+    return _username;
+}
+
+const std::string& Client::getMode(void)const{ 
+    return _mode;
+}
+
+const std::string& Client::getPass(void)const{
+    return _passwd;
+}
+
+const std::string& Client::getServ(void)const{
+    return _servername;
+}
+
+const time_t* Client::lastActiveWhen(void){
+    return _lastActive;
+}
+
+const bool    Client::getStatus(void)const{
+    return _status;
+}
+
+const bool    Client::isAuth(void)const{
+    return _isAuth;
+}
+
+const bool    Client::isUserd(void)const{
+    return _isUserd;
+}
+
+const bool    Client::isOper(void)const{
+    return _isOper;
+}
+
+const Channel* Client::getCurrChan(void)const{
+    return _currChan;
+}
+
+void Client::setFd(int fd){
+    _fd = fd;
+}
+
+void Client::setHost(std::string& host){
+    _hostname = host;
+}
+
+void Client::setNick(std::string& nick){
+    _nickname = nick;
+}
+
+void Client::setUser(std::string& user){
+    _username = user;
+}
+
+void Client::setMode(std::string& mode){
+    _mode = mode; 
+}
+
+void Client::setPass(std::string& pass){
+    _passwd = pass; 
+}
+
+void Client::setServ(std::string& serv){
+    _servername = serv;
+}
+
+void Client::setLastActive(time_t& when){
+    _lastActive = &when;
+}
+
+void Client::setStatus(void){
+    _status = true;
+}
+
+void Client::setIsAuth(void){
+    _isAuth = true;
+}
+void Client::setIsUser(void){
+    _isUserd = true;
+}
+void Client::setIsOper(void){
+    _isOper = true;
+}
+void Client::setCurrChan(Channel& curr){
+    _currChan = &curr;
+}
