2dfb84edbb057bfffd5b425778d1cc8ae118c708
adilsxn <24adilsonnunes@gmail.com>
2024-11-03 23:02:00 +0000

    add: commands header

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -2,6 +2,12 @@
 #define COMMANDS
 
 #include "Client.hpp"
+#include "Message.hpp"
+
+
+void passCommand(Client& cli, Message& msg);
+void nickCommand(Client& cli, Message& msg);
+void userCommand(Client& cli, Message& msg);
 
 
 
