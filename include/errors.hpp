761ffb639d77b51d96d74746fb7d83ac1811698b
adilsxn <24adilsonnunes@gmail.com>
2024-11-11 11:04:42 +0000

    adding errors

--- include/errors.hpp
+++ include/errors.hpp
@@ -1,12 +1,36 @@
 #ifndef ERRORS
 #define ERRORS
 
+#pragma once
 #include <string>
-#include "Client.hpp"
 
-void show_usage(void);
 std::string errmap(int errcode);
 void send_error(Client& cli, int errnum, std::string& arg);
+// REPLIES
+# define RPL_WELCOME(nick, user, host) (":Welcome to the Internet Relay Network " + nick + "!" + user + "@" + host + "\r\n")
+# define RPL_YOURHOST(servername, version) (":Your host is " + servername + ", running version " + version + "\r\n")
+# define RPL_CREATED(date) (":This server was created " + date + "\r\n")
+# define RPL_MYINFO(servername, version, available_user_modes, available_channel_modes) \
+	(":" + servername + " " + version + " " + available_user_modes + " " + available_channel_modes + "\r\n")
+# define RPL_MOTDSTART(server) (":- " + server + " Message of the day - \r\n")
+# define RPL_MOTD(txt) (txt + "\r\n")
+# define RPL_ENDOFMOTD() (":End of /MOTD command\r\n")
+# define RPL_UMODEIS(user_mode) (user_mode + "\r\n")
+# define RPL_WHOREPLY(curr_chan, username, hostname, servname, nick, ircoper, chanoper, realname) \
+	(curr_chan + " " + username + " " + hostname + " " + servname + " " + nick + " H" + ircoper \
+	+ chanoper + " :0 " + realname + "\r\n")
+# define RPL_ENDOFWHO(name) (name + " :End of /WHO list\r\n")
+# define RPL_CHANNELMODEIS(channel, mode) (channel + " :+" + mode + "\r\n")
+# define RPL_NOTOPIC(channel) (channel + " :No topic is set" + "\r\n")
+# define RPL_TOPIC(channel, topic) (channel + " :" + topic + "\r\n")
+# define RPL_TOPICWHOTIME(channel, who, when) (channel + " " + who + " :" + when + "\r\n")
+# define RPL_NAMREPLY(channel, list) ("= " + channel + " :" + list + "\r\n")
+# define RPL_ENDOFNAMES(channel) (channel + " :End of /NAMES list.\r\n")
+# define RPL_AWAY(nick, msg) (nick + " :" + msg + "\r\n")
+# define RPL_CREATIONTIME(channel, creation_time) (channel + " :" + creation_time + "\r\n")
+# define RPL_BANLIST(channel, mask) (channel + " :" + mask + "\r\n")
+# define RPL_ENDOFBANLIST(channel) (channel + " :End of channel ban list\r\n")
+# define RPL_INVITING(guest, channel) (guest + " :" + channel + "\r\n")
 
 // ERRORS
 # define ERR_NOSUCHNICK			401
@@ -44,5 +68,7 @@ void send_error(Client& cli, int errnum, std::string& arg);
 # define ERR_NOOPERHOST			491
 # define ERR_UMODEUNKNOWNFLAG	501
 # define ERR_USERSDONTMATCH		502
+
+# define ERR_SERVERISFULL(host) ("ERROR :Closing link: (unknown@" + host + ") [No more connections allowed from your host via this connect class (local)]\r\n")
 
 #endif // !ERRORS
