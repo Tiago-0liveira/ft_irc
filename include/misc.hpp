85646f9122eeb513f12fb0ff14352297729671fc
adilsxn <24adilsonnunes@gmail.com>
2024-11-26 11:24:32 +0000

    add: strSplit to misc

--- include/misc.hpp
+++ include/misc.hpp
@@ -7,9 +7,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string>
+#include <vector>
 #include <sys/socket.h>
 
 int         parse_port(char* str, bool& error);
+std::vector<std::string> strSplit(std::string const& s, char delim);
 std::string format(const std::string messageFormat, ...);
 // TODO: this should be replaced in the future
 void sendMessage(int clientSocket, const std::string& message);
