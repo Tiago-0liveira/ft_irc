08618586cb5847a3590e399f6c1cc508d782cecd
adilsxn <24adilsonnunes@gmail.com>
2024-11-28 02:40:16 +0000

    add: msgOfTheDay

--- include/misc.hpp
+++ include/misc.hpp
@@ -7,13 +7,31 @@
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-#include <vector>
 #include <sys/socket.h>
+#include <vector>
 
-int         parse_port(char* str, bool& error);
+class Server;
+class Client;
+#define SERVER_NAME "mariairc"
+#define DEFAULT_HOST "127.0.0.1"
+#define AVAILABLE_USER_MODES "iswo"
+#define AVAILABLE_CHANNEL_MODES "opitlb"
+#define BACKLOG 5
+#define MAXCLI 5
+#define BUFSIZE 128
+#define SERVER_VERSION "11.27.24"
+#define MAX_CHAN_PER_USR 10
+#define MAX_USR_PER_CHAN 10
+#define MAX_USR_NICK_LEN 9
+#define MAX_CHAN_NAME_LEN 200
+
+int                      parse_port(char* str, bool& error);
 std::vector<std::string> strSplit(std::string const& s, char delim);
-std::string format(const std::string messageFormat, ...);
+std::string              format(const std::string messageFormat, ...);
 // TODO: this should be replaced in the future
 void sendMessage(int clientSocket, const std::string& message);
+void msgOfTheDay(Server& srv, Client& cli);
+#include <Client.hpp>
+#include <Server.hpp>
 
 #endif
--- src/misc.cpp
+++ src/misc.cpp
@@ -1,4 +1,6 @@
+#include "Client.hpp"
 #include <misc.hpp>
+#include <errors.hpp>
 
 int parse_port(char* str, bool& error)
 {
@@ -29,4 +31,14 @@ std::string format(const std::string messageFormat, ...)
 void sendMessage(int clientSocket, const std::string& message)
 {
     send(clientSocket, message.c_str(), message.length(), 0);
+}
+
+void msgOfTheDay(Server& srv, Client& cli){
+
+    send_reply(cli, 001, RPL_WELCOME(cli.getNick(), cli.getUser(), cli.getHost()));
+	send_reply(cli, 002, RPL_YOURHOST(srv.getName(), SERVER_VERSION));
+	send_reply(cli, 003, RPL_CREATED(srv.getDateOfCreation()));
+	send_reply(cli, 004, RPL_MYINFO(srv.getName(), SERVER_VERSION, AVAILABLE_USER_MODES, AVAILABLE_CHANNEL_MODES));
+
+    send_error(cli, ERR_NOMOTD, "");
 }
