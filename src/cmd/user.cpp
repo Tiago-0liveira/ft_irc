79bd2ad1f91c16fe97fe921f06e825aa055478ba
adilsxn <24adilsonnunes@gmail.com>
2024-11-28 02:39:15 +0000

    add protection for order of command user after nick plus msgOfTheDay

--- src/cmd/user.cpp
+++ src/cmd/user.cpp
@@ -1,6 +1,7 @@
 #include <Client.hpp>
 #include <Commands.hpp>
 #include <errors.hpp>
+#include <misc.hpp>
 #include <sstream>
 #include <string>
 
@@ -19,18 +20,20 @@ void userCommand(Client& cli, std::string& msg)
         send_error(cli, ERR_NEEDMOREPARAMS, cmd);
         return;
     }
-    else if (cli.isAuth())
+    else if (cli.isAuth() && cli.isPasswordSet() && cli.isReg())
     {
         send_error(cli, ERR_ALREADYREGISTRED, cmd);
         return;
     }
-    if (!cli.isAuth() && !cli.isPasswordSet())
-        return;
+    if (!cli.isAuth() || !cli.isPasswordSet())
+        return send_error(cli, ERR_NOTREGISTERED, cmd);
     cli.setUser(user);
     cli.setMode(mode);
     (void)unused;
     cli.setRealname(realname);
     cli.setReg();
     // TODO: MesssageOfTheDay func()
+    //
+    msgOfTheDay(*(cli.getServer()), cli);
     return;
 }
