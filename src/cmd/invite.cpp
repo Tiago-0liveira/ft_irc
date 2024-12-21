44bf563ed58e6fcaeadecd8e9427bfcc32209b49
adilsxn <24adilsonnunes@gmail.com>
2024-12-21 23:14:08 +0000

    add: new invite error handling

--- src/cmd/invite.cpp
+++ src/cmd/invite.cpp
@@ -15,17 +15,25 @@ void inviteCommand(Client& cli, std::string& msg)
     ss >> nick;
     ss >> channel;
 
-	if (!cli.isAuth() || !cli.isReg())
-	{
-		send_error(cli, ERR_NOTREGISTERED, cmd, false);
-		return;
-	}
+    if (!cli.isAuth() || !cli.isReg())
+    {
+        send_error(cli, ERR_NOTREGISTERED, cmd, false);
+        return;
+    }
     else if (nick.empty() || channel.empty())
         return send_error(cli, ERR_NEEDMOREPARAMS, cmd);
     Client*  invitedCli = ptr->findClient(nick);
     Channel* chan       = ptr->findChannel(channel);
     if (invitedCli == NULL)
         return send_error(cli, ERR_NOSUCHNICK, cmd);
+    else if (chan == NULL)
+        return send_error(cli, ERR_NOSUCHCHANNEL, cmd);
+    else if (chan->isMember(cli) == false)
+        return send_error(cli, ERR_NOTONCHANNEL, cmd);
+    else if (chan->isMember(*invitedCli) == false)
+        return send_error(cli, ERR_USERONCHANNEL, cmd);
+    else if (chan->getInviteOnly() == true && chan->isOp(cli) == false)
+        return send_error(cli, ERR_CHANOPRIVSNEEDED, cmd);
     chan->inviteClient(cli, *invitedCli);
     return;
 }
